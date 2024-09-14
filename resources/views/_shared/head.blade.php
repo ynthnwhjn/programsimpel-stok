a27e2e99e329d76b5ff4ea3405b8892910ba470d
Yonathan <yonathan.personal@gmail.com>
2024-09-15 01:00:43 +0700

    fix bug

--- resources/views/_shared/head.blade.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>| @yield('pageTitle')</title>
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-    <link rel="stylesheet" href="/css/app.css">
-</head>
